ddddf294244fef5ae73c42771bf407f89551ccc9
Sarnovskiy Daniil <99619436+BakerPizza@users.noreply.github.com>
2022-06-20 00:41:29 +0300

    Update connection.php

--- php/connection.php
+++ php/connection.php
@@ -1,4 +1,4 @@
 <?php
-$connect = mysqli_connect("localhost", "root","", "bd_name") or die ("error_connect_to_database(idi ti nahuy dalbaeb)". mysqli_error($conn));
+$connect = mysqli_connect("localhost", "root","", "bd_name") or die ("error_connect_to_database(ERROR 404)". mysqli_error($conn));
 $connect->set_charset("utf8");
 ?>
